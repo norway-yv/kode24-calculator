7519233642e52ecdd531992f0e757ed5cc69af22
norway-yv <94965821+norway-yv@users.noreply.github.com>
2021-11-29 10:11:14 +0100

    Loaded all data from csv

--- backend.py
+++ backend.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 data = pd.read_csv("data.csv", sep=",")
 age = data["Hva er din alder?"]
 county = data["I hvilket fylke ligger jobben din?"]
-
-plt.plot(age, county)
-plt.show()
+workSituation = data["Hva beskriver best din arbeidssituasjon?"]
+businessType = data["Er du ansatt i det offentlige eller private næringsliv?"]
+yearsEducation = data["Hvor mange års relevant, formell utdannelse har du?"]
+workExperience = data["Hvor mange års relevant arbeidserfaring har du?"]
+wage = data["Hva er din grunnlønn? (årslønn før skatt, uten eventuelle bonuser eller overtidsbetaling)"]
