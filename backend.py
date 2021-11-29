254a5465c0b1ff17fd9ed262352902e490adedc3
norway-yv <94965821+norway-yv@users.noreply.github.com>
2021-11-29 09:37:34 +0100

    Added csv reading

--- backend.py
+++ backend.py
@@ -1,2 +1,11 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+#Get all data
+data = pd.read_csv("data.csv", sep=",")
+age = data["Hva er din alder?"]
+county = data["I hvilket fylke ligger jobben din?"]
+
+plt.plot(age, county)
+plt.show()
