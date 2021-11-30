152cd44d89b23a458bd3e54abc2304d98dc4ac72
norway-yv <94965821+norway-yv@users.noreply.github.com>
2021-11-30 12:00:23 +0100

    Finished age categories and added respondents

--- backend.py
+++ backend.py
@@ -11,31 +11,24 @@ businessType = data["Er du ansatt i det offentlige eller private næringsliv?"]
 yearsEducation = data["Hvor mange års relevant, formell utdannelse har du?"]
 workExperience = data["Hvor mange års relevant arbeidserfaring har du?"]
 wage = data["Hva er din grunnlønn? (årslønn før skatt, uten eventuelle bonuser eller overtidsbetaling)"]
-
+#Make data into list
 age , county, workSituation, businessType, yearsEducation, workExperience = age.tolist(), county.tolist(), workSituation.tolist(), businessType.tolist(), yearsEducation.tolist(), workExperience.tolist()
-age2, county2, workSituation2, businessType2, yearsEducation2, workExperience2 = age, county, workSituation, businessType, yearsEducation, workExperience
 
-"""
-for i in range(len(age)-1):
-    age2.remove(age[i])
-    age2.append(age[i][:-2])
-    county2.remove(county[i])
-    county2.append(county[i])
-    workSituation2.remove(workSituation[i])
-    workSituation2.append(workSituation[i])
-    businessType2.remove(businessType[i])
-    businessType2.append(businessType[i])
-    yearsEducation2.remove(yearsEducation[i])
-    yearsEducation2.append(yearsEducation[i])
-    workExperience2.remove(workExperience[i])
-    workExperience2.append(workExperience[i])
-print(age2)
-"""
+#Create dictionaries for search
+    #Age categories with lists of ages
 ageCategories = {
-    "20-24": range(20, 24).tolist()
-    "25-29": range(25, 29).tolist()
-    "30-34": range(30, 34).tolist()
-    "35-39": range(35, 39).tolist()
-    "40-44": range(40, 44).tolist()
-    "45-50": range(45, 50).tolist()
-}
+    "20-24": [*range(20, 25)],
+    "25-29": [*range(25, 30)],
+    "30-34": [*range(30, 35)],
+    "35-39": [*range(35, 40)],
+    "40-44": [*range(40, 45)],
+    "45-50": [*range(45, 51)],
+    "50+": [*range(50, 100)],
+    }
+    #Create individual data strings for respondents
+respondents = {}
+for i in range(len(age)):
+    toAdd = {0:{}}
+    respondents.update(toAdd)
+    toAdd = {"Age":age[i], "County":county[i], "Work Situation":workSituation[i], "Business Type":businessType[i], "Years Education":yearsEducation[i], "Work Experience":workExperience[i]}
+    respondents[i].update(toAdd)
